a934dd402f1b5a01ba5aaf9771e10d7b7de7101d
Daniel Tapia <danieltapia.g0126@gmail.com>
2021-12-15 14:19:30 -0600

    ejercicios semana1

---
+++ javascript/cuentame.js
@@ -0,0 +1,8 @@
+let nombre = "danielTapia";
+let numerofavorito = 26;
+let tienesmascotas = "true";
+
+
+console.log(nombre);
+console.log(numerofavorito);
+console.log(tienesmascotas);
---
+++ javascript/main.js
@@ -0,0 +1,60 @@
+//alert("hola mundo"); comentario 
+/*
+esto permite hacer un comentario más largo
+*/
+/*
+palabras reservadas
+var
+const
+let
+*/
+
+let miNombre = "Daniel Tapia";
+
+console.log(miNombre);
+
+/*
+number
+10
+10.0
+-50.0
+*/
+let numero1 = 10; 
+let numero2 = 10.05;
+let numero3 = -50.0; 
+
+console.log(numero1, numero2, numero3);
+console.log("el valor de numero 1 es: " + numero1);
+console.log("el valor de numero 2 es: " + numero2);
+console.log("el valor de numero 3 es: " + numero3);
+
+/*
+Boolean
+True = verdadero
+False = Falso
+*/
+
+let miBoolean1 = true; 
+let miBoolean2 = false;
+
+console.log("el valor de numero 3 es 1 es: " + miBoolean1);
+console.log("el valor de numero 3 es 1 es: " + miBoolean2);
+
+/*
+null
+la variable esta vacia 
+*/
+
+let variableVacia = null; 
+
+console.log(variableVacia);
+
+/*
+undefined
+*/
+
+let variable;
+console.log(variable);
+
+variable = "hola"; 
+console.log(variable);
